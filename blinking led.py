1210d89570d2554230b83ee9ef217aae4551729b
Nishanth D Almeida <nd.almeida19@gmail.com>
2019-03-11 21:31:01 +0100

    Version2 Signed-off-by: Nishanth D Almeida <nd.almeida19@gmail.com>

--- blinking led.py
+++ blinking led.py
@@ -1,42 +1,70 @@
-import json
-import sys
-import urllib2
-import time
 import serial
-# Configurations
-ping_server = 30
-jenkins_jobs=["Myproject1"] 
-ser = serial.Serial('COM4', 9600) 
-# Arduino Configuration
-SUCCESS = 'b'
+import time
+import sys
+from jenkinsapi import jenkins
+
+
+# Configuration
+JENKINS_URL = 'http://localhost:8080/job/Myproject1/configure'
+DEVICE_PATH = 'COM4'
+BAUD_RATE = 9600
+
+SUCCESS = 'g'
 FAILURE = 'r'
-BUILDING = 'a'
-UNSTABLE = 'y'
-time.sleep(5)
-def get_status(jobName):
-    jenkinsUrl = "http://localhost:8080/job/Myproject1/"
+BUILDING = 'y'
+QUERY_FREQUENCY = 30 # Frequency to query jenkins, in seconds.
+
+
+# The view name to filter by. If None, then check all jobs.
+VIEW_NAME = None
+
+# Connect to the Arduino board.
+ser = serial.Serial(DEVICE_PATH, BAUD_RATE)
+
+# Sleep to allow Arduino board to configure serial communication.
+time.sleep(2)
+
+j = jenkins.Jenkins(JENKINS_URL)
+current_state = ''
+
+
+def change_light(state):
+    global current_state
+    if state != current_state:
+        ser.write(state)
+        current_state = state
+
+
+def check_jenkins():
     try:
-        jenkinsStream   = urllib2.urlopen( jenkinsUrl + jobName + "/lastBuild/api/json" )
-    except urllib2.HTTPError, e:
-        print "URL Error: " + str(e.code)
-        print "      (job name [" + jobName + "] probably wrong)"
-        sys.exit(2)
-    try:
-        buildStatusJson = json.load( jenkinsStream )
+        if VIEW_NAME:
+            job_names = j.views[VIEW_NAME].keys()
+
+        jobs = j.get_jobs()
+        for (name, job) in jobs:
+            if not job.is_enabled():
+                continue
+            if job_names:
+                if name not in job_names:
+                    continue
+            if not job._data['lastBuild']:
+                # If a job has never been built, skip it.
+                continue
+            last_build = job.get_last_build()
+            if last_build and last_build.is_running():
+                change_light(BUILDING)
+                return
+            elif not job.get_last_build().is_good():
+                change_light(FAILURE)
+                return
+        change_light(SUCCESS)
     except:
-        print "Failed to parse json"
-        sys.exit(3)
-    return jobName,buildStatusJson["timestamp"], buildStatusJson["result"],
-while(1):
-    for job in jenkins_jobs:
-        status = get_status(job)
-#        print status[0], status[2]
-        if status[2] == "UNSTABLE":
-            ser.write(UNSTABLE)
-        elif status[2] == "SUCCESS":
-            ser.write(SUCCESS)
-        elif status[2] == "FAILURE":
-            ser.write(FAILURE)
-        elif status[2] == None:
-            ser.write(BUILDING)
-        time.sleep(ping_server)
+        change_light(FAILURE)
+        print "Unable to query jenkins:", sys.exc_info()[0]
+
+
+if __name__ == "__main__":
+    while(1):
+        # Main application loop.
+        check_jenkins()
+        time.sleep(QUERY_FREQUENCY)
